22712febb8f93e5da8118916e60efd2168ebaf17
Tatiana Smirnova <redkiivid@mail.ru>
2023-03-18 18:26:24 +0300

    ReformatCode commit

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-       double weight = 70;
-        double height = Math.pow (1.8, 2);
-        double bmiIndex = service.calculate (weight,height);
+        double weight = 70;
+        double height = Math.pow(1.8, 2);
+        double bmiIndex = service.calculate(weight, height);
 
         System.out.println("Ваш индекс массы тела:" + bmiIndex);
     }
