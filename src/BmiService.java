5f7d3bb1e145edd1f52370ddf1119ce103c92c94
Tatiana Smirnova <redkiivid@mail.ru>
2023-03-20 19:25:13 +0300

    correct commit

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
     public double calculate(double weight, double height) {
-        double bmiIndex = weight / height;
+        double bmiIndex = weight / Math.pow(height, 2);
         return (int) bmiIndex;
     }
 }
